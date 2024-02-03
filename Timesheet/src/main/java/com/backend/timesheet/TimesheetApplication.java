70fc0aac4c09deab1916dde5b70b4a52af4ca7e3
Baganda49 <abdessalemsaa@gmail.com>
2024-02-03 10:50:02 +0100

    create project Timesheet_backEnd

---
+++ Timesheet/src/main/java/com/backend/timesheet/TimesheetApplication.java
@@ -0,0 +1,13 @@
+package com.backend.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimesheetApplication.class, args);
+    }
+
+}
---
+++ Timesheet/src/test/java/com/backend/timesheet/TimesheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
